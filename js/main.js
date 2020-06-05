cf1391ffe022a90ccfeddf074c2012f006451598
Mwikala Kangwa <hello@mwikala.co.uk>
2020-06-05 21:43:23 +0100

    Inital commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(() => {
+
+    // init slideshow
+    var slideShow = new stackedCards({
+        selector: '.slideshow',
+        transformOrigin: 'center'
+    });
+    slideShow.init();
+
+});
